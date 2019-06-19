42ccb772ab02d42709b2abb7e95caf0a072afd89
hunkel <kolan19988@yandex.ru>
2019-06-19 23:44:48 +0300

    add task with timer

--- app/src/main/java/ru/hunkel/servicesipc/services/TestService.kt
+++ app/src/main/java/ru/hunkel/servicesipc/services/TestService.kt
@@ -7,11 +7,11 @@ import android.os.IBinder
 import android.os.Looper
 import android.os.Message
 import android.util.Log
+import java.util.*
 
 class TestService : Service() {
 
     private var mHandler: ServiceHandler? = null
-
 
     /*
         INNER CLASSES
@@ -34,18 +34,33 @@ class TestService : Service() {
             Looper.prepare()
             mHandler = ServiceHandler()
             Looper.loop()
-            Log.d("T","looped")
+            Log.d("T", "looped")
         }
-
-
     }
 
+    private class NumGenerateTask : TimerTask() {
+        private val TAG = this::class.java.simpleName
+
+        override fun run() {
+            val randomNum = kotlin.random.Random.nextInt(0,100).toString()
+            Log.i(TAG, randomNum)
+        }
+    }
+
+    /*
+        FUNCTIONS
+    */
     override fun onCreate() {
         super.onCreate()
-
         val looper = ServiceThread()
 
+        val task = NumGenerateTask()
+
+        with(Timer()){
+            schedule(task,1000L)
+        }
     }
+
     override fun onBind(intent: Intent?): IBinder? {
 //        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         return null
