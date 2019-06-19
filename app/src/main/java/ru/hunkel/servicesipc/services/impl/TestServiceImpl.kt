a04350260fe190e384e2af40901321980a21bc21
hunkel <kolan19988@yandex.ru>
2019-06-19 22:52:12 +0300

    init service implementation

---
+++ app/src/main/java/ru/hunkel/servicesipc/services/impl/TestServiceImpl.kt
@@ -0,0 +1,9 @@
+package ru.hunkel.servicesipc.services.impl
+
+import android.app.Service
+import android.content.Intent
+import android.os.*
+import android.util.Log
+import ru.hunkel.servicesipc.ITestService
+
+class TestServiceImpl : ITestService.Stub()
