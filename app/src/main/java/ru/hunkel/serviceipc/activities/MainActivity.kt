2fac00c9666f001db65d1b2ab9d9185ae795633f
hunkel <kolan19988@yandex.ru>
2019-06-20 16:50:56 +0300

    add request permissions

--- app/src/main/java/ru/hunkel/serviceipc/activities/MainActivity.kt
+++ app/src/main/java/ru/hunkel/serviceipc/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.hunkel.serviceipc.activities
 
+import android.Manifest
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
@@ -12,7 +15,10 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.hunkel.servicesipc.IPasswordGenerator
 import ru.hunkel.servicesipc.R
+import ru.hunkel.servicesipc.services.LocationService
 import ru.hunkel.servicesipc.services.PasswordGeneratorService
+
+const val REQUEST_CODE_PERMISSIONS = 0
 
 class MainActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
@@ -69,9 +75,11 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, PasswordGeneratorService::class.java)
 
         isBound = if (isBound) {
+            stopLocationService()
             unbindService(serviceConnection)
             false
         } else {
+            acceptPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
             bindService(
                 intent,
                 serviceConnection,
@@ -79,6 +87,50 @@ class MainActivity : AppCompatActivity() {
             )
         }
         updateUI()
+    }
+
+    private fun startLocationService() {
+        startService(Intent(this, LocationService::class.java))
+
+    }
+
+    private fun stopLocationService() {
+        stopService(Intent(this, LocationService::class.java))
+
+    }
+
+    private fun acceptPermissions(permissions: Array<String>) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    startLocationService()
+                } else {
+                    //TODO make check for all permission requests
+                    if (shouldShowRequestPermissionRationale(permission)) {
+                        Toast.makeText(this, "Location permission needed for tracking", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            requestPermissions(permissions, REQUEST_CODE_PERMISSIONS)
+        } else {
+            //TODO do something cos the permission is already accepted on API<23
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE_PERMISSIONS -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationService()
+                } else {
+                    Toast.makeText(this, "Location permission needed for tracking", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        }
+
     }
 
     private fun updateUI() {
